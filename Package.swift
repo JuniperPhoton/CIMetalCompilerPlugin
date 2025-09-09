239316fdc933050b4ea41563a65622be008c22dd
JuniperPhoton <dengweichao@hotmail.com>
2025-09-09 14:24:25 +0800

    Adopt Swift Subprocess

--- Package.swift
+++ Package.swift
@@ -5,17 +5,22 @@ import PackageDescription
 
 let package = Package(
     name: "CIMetalCompilerPlugin",
+    platforms: [
+        .macOS(.v13)
+    ],
     products: [
         .plugin(name: "CIMetalCompilerPlugin", targets: ["CIMetalCompilerPlugin"]),
         .executable(name: "CIMetalCompilerTool", targets: ["CIMetalCompilerTool"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.6.1"),
+        .package(url: "https://github.com/swiftlang/swift-subprocess.git", branch: "main"),
     ],
     targets: [
         .plugin(name: "CIMetalCompilerPlugin", capability: .buildTool(), dependencies: ["CIMetalCompilerTool"]),
         .executableTarget(name: "CIMetalCompilerTool", dependencies: [
             .product(name: "ArgumentParser", package: "swift-argument-parser"),
+            .product(name: "Subprocess", package: "swift-subprocess")
         ]),
         .testTarget(
             name: "CIMetalCompilerPluginTests",
